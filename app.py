918c1a1875b2b88eae132ae8155775183cf99811
Allan Moreira <allan.vilacio@gmail.com>
2023-07-03 17:02:07 -0300

    Finalizando o filtro por estado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, Output, Input
+from dash import Dash, html, dcc, Output, Input, callback_context
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
@@ -21,32 +21,32 @@ app.layout = dbc.Container([
             html.Div([
                 html.H5('Evolução do COVID-19'),
                 dbc.Button(id='location-button', children='Brasil', color='primary', size='lg')
-            ]),
-            html.P('informe a data na qual deseja obter as informações', style={'margin-top':'30px'}),
+            ], style={'padding': '20px 0px'}),
+            html.P('informe a data na qual deseja obter as informações'),
             dcc.DatePickerSingle(id='data-select', 
                                 min_date_allowed=df['data'].min(),
                                 max_date_allowed=df['data'].max(),
                                 date=df['data'].max(),
-                                display_format='MMMM D, YYYY', style={"border":"opx solid black"}),
+                                display_format='MMMM D, YYYY'),
             dbc.Row([
                 dbc.Col(
                     dbc.Card(
                         dbc.CardBody([
                             html.Samp('Casos Acumulados'),
-                            html.H3(id='casos-acumulados-data'),
+                            html.H3(id='casos-acumulados-data', style={"color": "#FFFF66"}),
                             html.Samp('Novos casos na data'),
-                            html.H5(id='casos-data')
-                        ])
+                            html.H5(id='casos-data', style={"color": "#FFFF66"})
+                        ], style={'padding':'10px'})
                     )
                 ),
                 dbc.Col(
                     dbc.Card(
                         dbc.CardBody([
                             html.Samp('Óbitos Acumulados'),
-                            html.H3(id='obitos-acumulados-data'),
+                            html.H3(id='obitos-acumulados-data', style={"color": "#DF2935"}),
                             html.Samp('Óbitos na data'),
-                            html.H5(id='obitos-data')
-                        ])
+                            html.H5(id='obitos-data', style={"color": "#DF2935"})
+                        ], style={'padding':'10px'})
                     )
                 )
             ]),
@@ -59,6 +59,20 @@ app.layout = dbc.Container([
         ], md=7)    
     ])
 ], fluid=True)
+
+@app.callback(
+        Output('location-button', 'children'),
+        [
+            Input('graph-map', 'clickData'),
+            Input('location-button', 'n_clicks')
+        ]
+    )
+def update_location_button(clickData, n_clicks):
+    evento = callback_context.triggered[0]['prop_id']
+    location = 'BRASIL'
+    if evento == 'graph-map.clickData':
+        location = clickData['points'][0]['location']
+    return location
 
 @app.callback(
     Output('graph-map','figure'),
@@ -82,15 +96,24 @@ def graph_map(data):
 
 @app.callback(
     Output('graph-bar', 'figure'),
-    Input('select-type', 'value')
+    [
+        Input('select-type', 'value'),
+        Input('location-button', 'children')
+    ]
 )
-def graph_col_1(select_type):
-    df_fig = df.groupby('data')[select_type].sum()
-    fig = go.Figure(go.Scatter(x=df_fig.index, y=df_fig))
+def graph_col_1(select_type, location):
+    df_fig = df.copy() if location=='BRASIL' else df[df['estado']==location]
+    df_fig = df_fig.groupby('data')[select_type].sum()
+    fig = go.Figure()
+    if select_type in ['casosAcumulado', 'obitosAcumulado']:
+        fig.add_trace(go.Scatter(x=df_fig.index, y=df_fig))
+    else:
+        fig.add_trace(go.Bar(x=df_fig.index, y=df_fig))
+
     fig.update_layout(autosize=True,
                       margin={'l':10,'t':10, 'r':10, 'b':10},
                       paper_bgcolor='#242424', plot_bgcolor='#242424',
-                      height=350)
+                      height=400)
     return fig
 
 @app.callback(
@@ -100,16 +123,22 @@ def graph_col_1(select_type):
         Output('obitos-acumulados-data','children'),
         Output('obitos-data','children'),
     ],
-    Input('data-select', 'date')
+    [
+        Input('data-select', 'date'),
+        Input('location-button', 'children')
+    ]
 )
-def update_cards(data):
+def update_cards(data, location):
+    data='2022-07-05'
     df_filtered = df[df['data']==data]
+    df_filtered = df_filtered if location=='BRASIL' else df_filtered[df_filtered['estado']==location]
+    df_filtered = (df_filtered[['casosAcumulado', 'casosNovos', 'obitosAcumulado', 'obitosNovos']]
+                   .sum()
+                   .apply(lambda x: f'{int(x):,}'.replace(',','.')))
 
-    casos_acumulado = f"{int(df_filtered['casosAcumulado'].sum()):,}".replace(',','.')
-    casos_novos = f"{int(df_filtered['casosNovos'].sum()):,}".replace(',','.')
-    obitos_acumulado = f"{int(df_filtered['obitosAcumulado'].sum()):,}".replace(',','.')
-    obito_novos =  f"{int(df_filtered['obitosNovos'].sum()):,}".replace(',','.')
-    return casos_acumulado, casos_novos, obitos_acumulado, obito_novos
+    return (df_filtered['casosAcumulado'], df_filtered['casosNovos'], 
+            df_filtered['obitosAcumulado'], df_filtered['obitosNovos'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
